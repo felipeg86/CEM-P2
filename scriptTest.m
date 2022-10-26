50a787e2298b37e2cdf9809c882edb248c576bfc
Juan Felipe González Pardo <jugonzalezpa@unal.edu.co>
2022-10-25 21:48:23 -0500

    Creación de objetos
    Organización del código, inclusión de objetos

---
+++ scriptTest.m
@@ -0,0 +1,16 @@
+clc; close all;
+
+obj = PEC('Shapes\Surf_3.obj');
+%element = obj.readObj();
+core = Core();
+core.addElements()
+
+%obj = readObj('Shapes\Surf_3.obj');
+%obj2 = readObj('Shapes\Surf_2.obj');
+
+%objs = [obj, obj2];
+
+%disp(length(objs))
+
+%plotElements(obj.element)
+
