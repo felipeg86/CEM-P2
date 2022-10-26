8c990f21e556f5edd4ce58fde5b721b8ebe9f9c4
Juan Felipe González Pardo <jugonzalezpa@unal.edu.co>
2022-10-25 23:43:20 -0500

    Update scriptTest.m

--- scriptTest.m
+++ scriptTest.m
@@ -3,12 +3,5 @@ clc; close all; clear;
 simulation = Simulation();
 simulation.patchElements();
 
-%obj = readObj('Shapes\Surf_3.obj');
-%obj2 = readObj('Shapes\Surf_2.obj');
 
-%objs = [obj, obj2];
-
-%disp(length(objs))
-
-%plotElements(obj.element)
 
